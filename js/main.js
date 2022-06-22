b7fef22e5b9fb5be6d49696cf081116c874a6339
fariasleo1983 <leonardo.fariasrj@outlook.com>
2022-06-21 21:36:47 -0300

    Jquery e Ajax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert("funcionou");
+function consultaCep() {
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response) {
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("uf").innerHTML = response.uf;
+            document.getElementById("ddd").innerHTML = response.ddd;
+            //alert(response.logradouro);
+        }
+
+    })
+}
